33ee54fa454d158ff717b8357891759c3fd3e8c9
Carolien C.H.M. Maas <96486516+CHMMaas@users.noreply.github.com>
2026-01-07 13:49:31 +0100

    Add files via upload

---
+++ 0 - Data preparation.R
@@ -0,0 +1,226 @@
+################################################################################
+### Decision for dialysis versus conservative care
+### PART 0 - Data preparation
+################################################################################
+
+# remove history
+rm(list = ls(all.names = TRUE))
+
+# load data
+setwd("P:/SCREAM2/SCREAM2_Research/Carolien Maas/")
+load("Data/cleaned/snr_ckd.Rdata")        # CKD patients
+load("Data/cleaned/snr_rrt.Rdata")        # Renal Replacement Therapy (transplantation)
+load("Data/cleaned/snr_hdpd.Rdata")       # lab data from hemodialysis or peritoneal dialysis patients
+load("Data/cleaned/snr_death.Rdata")      # vital status
+
+# load functions
+source("Code/utils/data_manipulation.R")
+
+# set system to English (US), to format dates correctly
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+
+# load libraries
+library(data.table)
+
+################################################################################
+### Select relevant variables from CKD file ####################################
+################################################################################
+# Convert to data.table
+setDT(SOS_CKDFINAL2024)
+
+# Select relevant columns and filter non-missing birthdate
+ckd <- SOS_CKDFINAL2024[, c(
+  "LOPNR",
+  "decision_date1",
+  "decision_date2",
+  "decision_date3",
+  "decision_modality1",
+  "decision_modality2", 
+  "decision_modality3",
+  "decision_tx1",
+  "decision_tx_date1",
+  "info_date1",
+  "info_type1",
+  "birthdate",
+  "deathdate",
+  "female",
+  "visit_date",
+  "visittype",
+  "screa",
+  "sbp",
+  "dbp",
+  "calcium_total",
+  "phosphate",
+  "albumin",
+  "hb",
+  # heartdisease_other,
+  # malignancy_hematol,
+  # malignancy_other,
+  # cerebrovascular,
+  # diabetes,
+  # hypertension,
+  # ischemic_heartdisease,
+  # peripheral_arterial,
+  "esa",
+  # diuretic,
+  # phosbinder,
+  # vitamind,
+  "iron_med",
+  "iron_type",
+  "prd_cat",
+  "clinic",
+  "county"
+)]
+
+# convert to date
+ckd[, `:=`(
+  birthdate = as.IDate(birthdate, format("%d%b%Y")),
+  DODSDAT = as.IDate(deathdate, format("%m/%d/%Y")),
+  decision_date1 = as.IDate(decision_date1, format("%m/%d/%Y")),
+  decision_date2 = as.IDate(decision_date2, format("%m/%d/%Y")),
+  decision_date3 = as.IDate(decision_date3, format("%m/%d/%Y")),
+  decision_tx_date1 = as.IDate(decision_tx_date1, format("%m/%d/%Y")),
+  visit_date = as.IDate(visit_date, format("%m/%d/%Y"))
+)]
+
+# only keep unique rows
+ckd <- ckd[, unique(.SD)]
+nrow(ckd) # N = 403070
+
+# Sort by LOPNR and visit_date
+setDT(ckd)
+setkey(ckd, LOPNR, visit_date)
+
+################################################################################
+### Attach information from other cohorts
+################################################################################
+# death information
+snr_death <- UT_R_DORS_123160_2023[, c("LOPNR", "DODSDAT", "ULORSAK")]
+setDT(snr_death)
+snr_death[, `:=`(DODSDAT = as.IDate(as.character(DODSDAT), format("%Y%m%d")))]
+
+# renal replacement therapy information
+snr_rrt <- unique(SOS_KRTDATA2024[, c(
+  "LOPNR",
+  "birthdate",
+  "clinic",
+  "county",
+  "deathdate",
+  "female",
+  "krt_start",
+  "krt_startdate",
+  "modality_cat",
+  "prd_cat"
+)])
+setDT(snr_rrt)
+setnames(snr_rrt, "modality_cat", "krt_modality")
+snr_rrt[, `:=`(
+  birthdate = as.IDate(birthdate, format("%d%b%Y")),
+  DODSDAT = as.IDate(deathdate, format("%m/%d/%Y")),
+  krt_startdate = as.IDate(krt_startdate, format("%d%b%Y"))
+)]
+
+# some IDs have multiple rows
+snr_rrt[, .N, by="LOPNR"][N>1]
+snr_rrt[LOPNR==18316963,]
+
+# only keep krt_start == 1 information, this does not delete IDs
+snr_rrt <- snr_rrt[krt_start == 1, ]
+
+# dialysis information, note to not use future information
+snr_hdpd <- unique(SOS_DIALYSISDATA2024[, c(
+  "LOPNR",
+  "birthdate",
+  "deathdate",
+  "female",
+  "vascular_access",
+  "visit_date"
+)])
+setDT(snr_hdpd)
+
+# Define dates and visittype
+snr_hdpd[, `:=`(
+  birthdate = as.IDate(birthdate, format("%d%b%Y")),
+  DODSDAT = as.IDate(deathdate, format("%m/%d/%Y")),
+  visit_date = as.IDate(visit_date, format("%m/%d/%Y"))
+)]
+
+# add data on vital status
+merged_ckd <- merge(ckd, 
+                    snr_death,
+                    by = "LOPNR",
+                    all.x = TRUE,
+                    suffixes = c("", ".death"))
+
+# add data on renal replacement therapy
+merged_ckd <- merge(
+  merged_ckd,
+  snr_rrt,
+  by = "LOPNR",
+  all.x = TRUE,
+  suffixes = c("", ".rrt")
+)
+
+# add data on birthdate and sex
+merged_ckd <- merge(
+  merged_ckd,
+  snr_hdpd,
+  by = c("LOPNR", "visit_date"),
+  all = TRUE,
+  suffixes = c("", ".hdpd")
+)
+
+# take the minimum of these merged variables
+colnames.merge <- c("clinic", "county", "prd_cat", "birthdate", "DODSDAT", "female")
+for (colname in colnames.merge) {
+  merged_ckd[, colname := do.call(pmin, c(.SD, na.rm = TRUE)),
+             .SDcols = patterns(paste0("^", colname))]
+}
+
+# Remove intermediate columns
+merged_ckd[, c(grep("\\.hdpd$", colnames(merged_ckd), value = TRUE),
+               grep("\\.rrt$", colnames(merged_ckd), value = TRUE)) := NULL]
+
+# Set key for efficient joins and lookups
+setkey(merged_ckd, LOPNR, visit_date)
+
+# Check missings
+colSums(is.na(merged_ckd))
+nrow(merged_ckd) # N = 576292
+
+################################################################################
+### Registration errors
+################################################################################
+# ensure birthdate is known
+merged_ckd <- merged_ckd[!is.na(birthdate)]
+nrow(merged_ckd) # N = 576292
+
+# ensure visit_date is before date of death or has not died yet
+merged_ckd <- merged_ckd[visit_date < DODSDAT | is.na(DODSDAT)]
+nrow(merged_ckd) # N = 576250
+
+################################################################################
+### Calculate age and eGFR at each visit
+################################################################################
+merged_ckd <- calculate_age(dt = merged_ckd,
+                            birthdate_col = "birthdate", 
+                            visitdate_col = "visit_date")
+
+# fill serum creatinine
+merged_ckd <- retrieve_past_info(
+  dt = merged_ckd,
+  dictionary = merged_ckd,
+  id_name = "LOPNR",
+  date_name = "visit_date",
+  var_name = "screa",
+  max_roll_days = 365.25
+)
+
+# calculate eGFR using the 2021 creatinine equation
+merged_ckd[, egfr2021 := ckd_epi_2021_cr(screa, age, female)]
+
+# transform hemoglobin g/l into mmol/l
+merged_ckd[, hb := hb * 0.6206]
+
+# save file
+save(merged_ckd, file = "P:/SCREAM2/SCREAM2_Research/Carolien Maas/Data/merged_ckd.Rdata")
